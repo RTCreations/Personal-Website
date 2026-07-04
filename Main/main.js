0c0a0dd87faf00b7b9dea8976feb2f6d0c6fbbbf
R.T Projects <135464651+RTCreations@users.noreply.github.com>
2026-07-04 13:51:15 -0500

    Added react functionality and a navigation bar

--- Main/main.js
+++
@@ -1,7 +0,0 @@
-const personSchema = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    "name": "Ryu Rivera",
-    "url": "https://github.com/RTCreations/Personal-Website",
-    "bio": "My name is Ryu Rivera, check out my personal website!"
-}
